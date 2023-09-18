18314f8168699caccad2f69025dc8df486d2bb2c
Kévin Roussel <kevin.roussel.54@sfr.fr>
2023-09-18 18:05:20 +0200

    Ajout d'un fichier INI pour configurer l'application

--- src/WinMover.c
+++ src/WinMover.c
@@ -24,7 +24,14 @@
  *
  * Version courante du programme WinMouse.
  */
-#define VERSION_PRG  "0.7 du 30/08/2023"
+#define VERSION_PRG  "0.7.7 du 18/09/2023"
+
+/**
+ * \def TAILLE_MAX_MSG
+ *
+ * Taille maximale d'un message affiché par ce programme.
+ */
+#define TAILLE_MAX_MSG  4096U
 
 
 /* == Messages affichés == */
@@ -42,13 +49,10 @@ static const char* const MSG_ERR_SYS =
 		"\n Message d'erreur systeme : " ;
 
 
-/* == Valeurs numériques & chaînes à ne pas traduire == */
-
-/* taille maximale d'un message affiché par ce programme */
-static const unsigned TAILLE_MAX_MSG = 4096U;
+/* == Valeurs numériques == */
 
 /* coordonnées à donner à la fenêtre indiquée */
-static const RECT coords_fen = {
+static const RECT COORDS_FEN_DEFAUT = {
 		.left = 41L,
 		.top = 0L,
 		.bottom = 687L,
@@ -56,13 +60,37 @@ static const RECT coords_fen = {
 };
 
 
+/* == Constantes chaînes de caractères à ne pas traduire ! == */
+
 /* classe de fenêtre à rechercher */
-static const WCHAR* const CLASSE_FEN_EXPLOR = L"CabinetWClass" ;
+static const WCHAR* const CLASSE_FEN_EXPLOR_DEFAUT = L"CabinetWClass" ;
+
+/* nom du fichier INI de configuration de ce programme */
+static const WCHAR* const NOM_FICHIER_CONFIG = L"WinMover.ini" ;
+/* nom de section contenant la classe de fenêtre à rechercher */
+static const WCHAR* const NOM_SECTION_CLASSE = L"Win_Class" ;
+/* nom de la valeur pour le nom de classe */
+static const WCHAR* const NOM_VAL_CLASSE = L"Name" ;
+/* nom de section contenant les coordonnées à donner aux fenêtres */
+static const WCHAR* const NOM_SECTION_COORDS = L"Win_Rect" ;
+/* nom de la valeur pour la gauche de la fenêtre */
+static const WCHAR* const NOM_VAL_GAUCHE = L"Left" ;
+/* nom de la valeur pour le haut de la fenêtre */
+static const WCHAR* const NOM_VAL_HAUT = L"Top" ;
+/* nom de la valeur pour le bas de la fenêtre */
+static const WCHAR* const NOM_VAL_BAS = L"Bottom" ;
+/* nom de la valeur pour le haut de la fenêtre */
+static const WCHAR* const NOM_VAL_DROITE = L"Right" ;
 
 
 /*========================================================================*/
 /*                           VARIABLES GLOBALES                           */
 /*========================================================================*/
+
+/* classe de fenêtre à rechercher */
+static WCHAR classe_fenetre[TAILLE_MAX_MSG] ;
+/* coordonnées à donner aux fenêtres trouvées */
+static RECT coords_fen ;
 
 /* nombre de fenêtre redimensionnées */
 static unsigned int nbFenRedim ;
@@ -109,6 +137,45 @@ MsgErreurSys (const char* nomFnct)
 }
 
 
+/* détermine les paramètres de fonctionnement du programme */
+static void
+DetermineParams (void)
+{
+	/* retrouve le chemin vers le fichier INI à lire */
+	WCHAR chemin_ini[MAX_PATH] ;
+	memset (chemin_ini, 0, MAX_PATH) ;
+	DWORD len = GetCurrentDirectoryW (MAX_PATH, chemin_ini) ;
+	wcsncat (chemin_ini, L"\\", MAX_PATH - len - 1) ;
+	wcsncat (chemin_ini, NOM_FICHIER_CONFIG, MAX_PATH - len - 2) ;
+	/* retrouve le nom de la classe à rechercher */
+	GetPrivateProfileStringW (NOM_SECTION_CLASSE,
+	                          NOM_VAL_CLASSE,
+	                          CLASSE_FEN_EXPLOR_DEFAUT,
+	                          classe_fenetre,
+	                          TAILLE_MAX_MSG,
+	                          chemin_ini) ;
+	/* détermine les coordonnées à donner aux fenêtres trouvées :
+	   lit les valeurs voulues dans le fichier INI une à une,
+	   en prenant les valeurs par défaut le cas échéant */
+	coords_fen.left = (LONG) GetPrivateProfileIntW (NOM_SECTION_COORDS,
+	                                                NOM_VAL_GAUCHE,
+	                                                COORDS_FEN_DEFAUT.left,
+	                                                chemin_ini);
+	coords_fen.top = (LONG) GetPrivateProfileIntW (NOM_SECTION_COORDS,
+	                                               NOM_VAL_HAUT,
+	                                               COORDS_FEN_DEFAUT.top,
+	                                               chemin_ini);
+	coords_fen.bottom = (LONG) GetPrivateProfileIntW (NOM_SECTION_COORDS,
+	                                                  NOM_VAL_BAS,
+	                                                  COORDS_FEN_DEFAUT.bottom,
+	                                                  chemin_ini);
+	coords_fen.right = (LONG) GetPrivateProfileIntW (NOM_SECTION_COORDS,
+	                                                 NOM_VAL_DROITE,
+	                                                 COORDS_FEN_DEFAUT.right,
+	                                                 chemin_ini);
+}
+
+
 /* redimensionne la fenêtre indiquée */
 static BOOL
 RedimFenetre (HWND hWndCible)
@@ -152,7 +219,7 @@ MainWindowEnumerated (HWND hWnd,
 	                         (int)TAILLE_MAX_MSG) ;
 
 	/* s'agit-il de la classe recherchée ? */
-	if (wcsncmp (strCls, CLASSE_FEN_EXPLOR, (size_t)len) == 0) {
+	if (wcsncmp (strCls, classe_fenetre, (size_t)len) == 0) {
 		RedimFenetre (hWnd) ;
 		nbFenRedim++ ;
 	}
@@ -174,6 +241,9 @@ main (int argc,
 	/* affiche "l'entête" du programme */
 	fprintf (stdout, FMT_MSG_A_PROPOS, TITRE_PRG, VERSION_PRG) ;
 	fflush (stdout) ;
+
+	/* retrouve les valeurs à donner aux paramètres */
+	DetermineParams () ;
 
 	/* énumère les fenêtres principales affichées,
 	   pour trouver celles à traiter (selon leur classe) */
